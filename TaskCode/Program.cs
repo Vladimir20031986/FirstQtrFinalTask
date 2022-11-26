98b718566290f0e1503999abfb3447f3b369b490
Vladimir Leonov <Lion-king1986@mail.ru>
2022-11-26 18:30:59 +0300

    Программа решения задачи

---
+++ TaskCode/Program.cs
@@ -0,0 +1,38 @@
+﻿Console.Clear();
+
+string[] array = new string[4] { "1234", "167", "-2", "dflgs" };
+int count = 0;
+for (int i = 0; i < array.Length; i++)
+{
+    if (array[i].Length <= 3)
+    {
+        count++;
+    }
+}
+string[] newarray = new string[count];
+ResArray(array, newarray);
+PrintArray(newarray);
+
+
+string[] ResArray(string[] array, string[] newarray)
+{
+    int count = 0;
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i].Length <= 3)
+        {
+            newarray[count] = array[i];
+            count++;
+        }
+    }
+    return newarray;
+}
+
+void PrintArray(string[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write($"{array[i]} ");
+    }
+    Console.WriteLine();
+}
